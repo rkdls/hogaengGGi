3b0301f884b64f952d34969b741742728772c0ac
rkdls <rkdls9@naver.com>
2017-02-04 13:54:38 +0900

    검색창 추가

--- src/search/SearchBar.js
+++ src/search/SearchBar.js
@@ -5,13 +5,56 @@
 import React, {Component} from 'react';
 
 class SearchBar extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            search: "",
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
+    }
+
+    handleChange(e) {
+        let nextState = {};
+        nextState[e.target.name] = e.target.value;
+        this.setState(nextState)
+    }
+
+    handleKeyPress(e) {
+        console.log('handleKeyPress');
+        if (e.charCode == 13) {
+            console.log('key pressed');
+            this.handleSearch();
+
+        }
+    }
+
+    handleSearch() {
+        console.log('handle Search start')
+    }
+
+
     render() {
         return (
             <div
-
                 className="SearchBar"
             >
-                검색창
+                <input
+                    placeholder="검색"
+                    name="search"
+                    type="TEXT"
+                    onChange={this.handleChange}
+                    value={this.state.search}
+                    onKeyPress={this.handleKeyPress}
+                />
+                <button
+                    type="submit"
+                    onClick={this.handleSearch}
+                >
+                    검색
+                </button>
+
             </div>
         );
     }
